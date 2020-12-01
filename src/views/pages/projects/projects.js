d4a8cc61ee3841068105412db7bbef0bcf62fde0
Conrad <36226614+ConradParker@users.noreply.github.com>
2020-12-01 20:53:18 +0100

    Update projects.js

--- src/views/pages/projects/projects.js
+++ src/views/pages/projects/projects.js
@@ -2,7 +2,7 @@ import utils from '../../../services/utils.js';
 
 export default {
   render: async () => {
-    utils.injectCss('/views/pages/prjects/projects.css');
+    utils.injectCss('/views/pages/projects/projects.css');
     return utils.getPage('projects');
   },
   after_render: async () => {},
